aeb990cd32c4b8e948a4ff2abc46ae92afd3d061
James Sinkler <jamessinkler713@gmail.com>
2022-02-06 10:57:20 -0700

    update to remove test code, using sync no-longer async

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 const fs = require("fs");
 const path = require("path");
 
-async function getRandomFiveLetterWord () {
+function getRandomFiveLetterWord () {
   let words = fs.readFileSync(path.resolve(__dirname,  "words_dictionary.json"), "utf8")
   words = JSON.parse(words)
   words = Object.keys(words)
   let fiveLetterWords = words.filter(word => word.length === 5)
-  // console.log(fiveLetterWords)
   let length = fiveLetterWords.length
   let randomFiveWord = fiveLetterWords[Math.floor(Math.random()*length)]
   return randomFiveWord
 };
 
-const asyncFunc = async()=> {
-  const result = await getRandomFiveLetterWord()
-  console.log(result)
-}
-asyncFunc()
 
 module.exports = { getFiveLetterWord: getRandomFiveLetterWord }
